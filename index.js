6f629c88c6ad0a3b92ad1dfa09baf1aa3a0528e7
Erik Kemperman <erikkemperman@gmail.com>
2017-08-03 12:40:56 +0200

    Update: Add resolveConstants export

--- index.js
+++ index.js
@@ -10,19 +10,55 @@ function createResolver(config, options) {
   options = options || {};
 
   var resolver = {
+    resolveConstant: resolveConstant,
     resolve: resolve,
   };
 
+
   // Keep constants separately
   var constants = {};
+
+  function resolveConstant(key) {
+    if (constants.hasOwnProperty(key)) {
+      return constants[key];
+    }
+
+    var definition = config[key];
+    // Ignore options that are not defined
+    if (!definition) {
+      return;
+    }
+
+    var option = options[key];
+
+    if (!!option || options.hasOwnProperty(key)) {
+      if (typeof option !== 'function') {
+        option = normalize.call(resolver, definition.type, option);
+        if (option != null) {
+          constants[key] = option;
+          return option;
+        }
+        // Fall through
+      } else {
+        return;
+      }
+    }
+
+    var fallback = definition.default;
+    if (option == null && typeof fallback !== 'function') {
+      constants[key] = fallback;
+      return fallback;
+    }
+  }
+
 
   // Keep requested keys to detect (and disallow) recursive resolution
   var stack = [];
 
   function resolve(key) {
-
-    if (constants.hasOwnProperty(key)) {
-      return constants[key];
+    var option = resolveConstant(key);
+    if (option != null) {
+      return option;
     }
 
     var definition = config[key];
@@ -37,14 +73,12 @@ function createResolver(config, options) {
 
     stack.push(key);
     try {
-      var option = options[key];
+      option = options[key];
       var appliedArgs = slice.call(arguments, 1);
       var args = [definition.type, option].concat(appliedArgs);
 
       if (typeof option === 'function') {
         option = normalize.apply(resolver, args);
-      } else {
-        option = null;
       }
 
       if (option == null) {
@@ -61,38 +95,7 @@ function createResolver(config, options) {
     }
   }
 
-
-  // Pre-process
-  options = Object.keys(config).reduce(function(opts, key) {
-
-    var definition = config[key];
-    var option = options[key];
-
-    if (!!option || options.hasOwnProperty(key)) {
-      if (typeof option !== 'function') {
-        option = normalize.call(resolver, definition.type, option);
-        if (option != null) {
-          constants[key] = option;
-          return opts;
-        }
-        // Fall through
-      } else {
-        opts[key] = option;
-        return opts;
-      }
-    }
-
-    var fallback = definition.default;
-    if (typeof fallback !== 'function') {
-      constants[key] = fallback;
-    }
-
-    return opts;
-  }, {});
-
-
   return resolver;
 }
-
 
 module.exports = createResolver;
